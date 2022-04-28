4410d8d9e155b647edcdde3195baca55a825e77f
Oliver <ychen939@wisc.edu>
2022-04-28 09:01:58 -0500

    fixed bug in producincg na in depth and distance

--- project_q3.R
+++ project_q3.R
@@ -16,9 +16,13 @@ colnames(data) <- c('source_origin_time','source_latitude','source_longitude',
     'source_depth_km','source_magnitude','source_distance_km')
 depth_avg <- mean(data$source_depth_km)
 dist_avg <- mean(data$source_distance_km)
-depth_filename <- paste('job2_depth_', data_num, sep='')
-depth_filename <- paste(depth_filename, '.txt', sep='')
-dist_filename <- paste('job2_dist_', data_num, sep='')
-dist_filename <- paste(dist_filename, '.txt', sep='')
-write.table(depth_avg, depth_filename)
-write.table(dist_avg, dist_filename)
+if (depth_avg != 'NA' && depth_avg != '') {
+    depth_filename <- paste('job2_depth_', data_num, sep='')
+    depth_filename <- paste(depth_filename, '.txt', sep='')
+    write(depth_avg, depth_filename)
+}
+if (dist_avg != 'NA' && dist_avg != '') {
+    dist_filename <- paste('job2_dist_', data_num, sep='')
+    dist_filename <- paste(dist_filename, '.txt', sep='')
+    write(dist_avg, dist_filename)
+}

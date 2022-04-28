1612b01ebd86ba70c5403a87f4562553fc2ed77a
yang558 <yang558@learn.chtc.wisc.edu>
2022-04-28 00:27:17 -0500

    Update job1postCount.sh

--- job1postCount.sh
+++ job1postCount.sh
@@ -2,5 +2,5 @@
 
 cat job1_*.csv | cut -d, -f3 | awk '{ printf("%d\n", $1) }' > latitude.txt
 cat job1_*.csv | cut -d, -f4 | awk '{ printf("%d\n", $1) }' > longitude.txt
-paste -d',' latitude.txt longitude.txt > location.txt
-sort location.txt | uniq -c | sort -nr > countByLocations
+paste -d',' latitude.txt longitude.txt | sort | uniq -c | sort -nr | tail -n +2 | head -10 > top10Locations
+#sort location.txt | uniq -c | sort -nr > countByLocations

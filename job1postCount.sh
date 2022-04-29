e161951762d53990d61047c5bab2e6863540d50f
Steven Yang <79009848+stevenYang914@users.noreply.github.com>
2022-04-29 00:13:55 -0500

    Delete job1postCount.sh

--- job1postCount.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#cat job1_*.csv | cut -d, -f3 | awk '{ printf("%d\n", $1) }' > latitude.txt
-#cat job1_*.csv | cut -d, -f4 | awk '{ printf("%d\n", $1) }' > longitude.txt
-#paste -d',' latitude.txt longitude.txt | sort | uniq -c | sort -nr | tail -n +2 | head -10 > top10Locations
-
-cat job1_*.csv | cut -d, -f3,4 | sort | uniq -c | sort -nr | head -10 > top10Locations
